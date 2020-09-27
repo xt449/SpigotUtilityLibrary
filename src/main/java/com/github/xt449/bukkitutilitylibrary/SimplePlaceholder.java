8c408911f753c83be615508d5c61c45719c25343
xt449 <alpha.xt449.omega@gmail.com>
2020-09-26 21:02:37 -0400

    WIP Placeholder system

---
+++ src/main/java/com/github/xt449/bukkitutilitylibrary/SimplePlaceholder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020 xt449/BinaryBanana
+ *
+ * This file is part of BukkitUtilityLibrary.
+ *
+ * BukkitUtilityLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BukkitUtilityLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BukkitUtilityLibrary.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.xt449.bukkitutilitylibrary;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class SimplePlaceholder {
+
+	private final String placeholder;
+
+	private String value = null;
+
+	public SimplePlaceholder(@NotNull String placeholder, String value) {
+		this.placeholder = placeholder;
+		this.value = value;
+	}
+
+	public SimplePlaceholder(@NotNull String placeholder) {
+		this.placeholder = placeholder;
+	}
+
+	@NotNull
+	public String getPlaceholder() {
+		return placeholder;
+	}
+
+	@Nullable
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
